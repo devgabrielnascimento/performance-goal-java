29006527d5a84cf27fce5532f357b335fb1d1c80
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-02-27 09:39:10 -0300

    menu adicionado ao projeto

--- Ecommerce/src/produto/Menu.java
+++ Ecommerce/src/produto/Menu.java
@@ -3,7 +3,25 @@ package produto;
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		String menu =
+					 "|                                   |"
+					 + "\n|       BEM-VINDO/A/E A EATMAIS     |" 
+				     + "\n|                                   |"
+				     + "\n|---------------------------------- |"
+					 +"\n|              [MENU|               |"
+					 + "\n|---------------------------------- |"
+					 + "\n| O QUE VOCÊ GOSTARIA DE FAZER?     |"
+					 + "\n| [1] CADASTRAR UM PRODUTO          |"
+					 + "\n| [2] ATUALIZAR UM PRODUTO          |"
+					 + "\n| [3] DELETAR UM PRODUTO            |"
+					 + "\n| [4] BUSCAR POR UM PRODUTO         |"
+					 + "\n| [5] LISTAR TODOS OS PRODUTOS      |"
+					 + "\n| [6] SAIR                          |";
+					 
+		
+	System.out.println(menu);
+	
 
 	}
 
